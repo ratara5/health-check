5d1cec5fc5c21e286a2209bfc3ed410b610cfca1
ratara5 <ratara5@gmail.com>
2024-02-08 19:02:31 -0500

    cleanup code

--- src/main/java/org/health/healthcheck/domain/repository/UserRepository.java
+++ src/main/java/org/health/healthcheck/domain/repository/UserRepository.java
@@ -10,7 +10,7 @@ import java.util.Optional;
 public interface UserRepository {
 
     List<User> getAll();
-    Optional<User> getUser(int productId);
+    Optional<User> getUser(int userId);
     User save(User user);
     void delete(int userId);
     UserProjection getImc(int userId);
